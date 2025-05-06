fcc365a961bedbc39ea86550e2b94e50add3c202
Rene Clark <31482962+rclark848@users.noreply.github.com>
2025-05-06 14:27:48 -0400

    fixed gen time

--- Scripts/Upstream_Analyses/Ela_momi_bootstrap_temponly.py
+++ Scripts/Upstream_Analyses/Ela_momi_bootstrap_temponly.py
@@ -7,7 +7,7 @@ sfs = momi.Sfs.load(sfsfile)
 NeConstant=1e4
 
 #specify model
-model_inf_2histchange_temponly =  momi.DemographicModel(N_e=NeConstant, gen_time=1, muts_per_gen=2.5e-8)
+model_inf_2histchange_temponly =  momi.DemographicModel(N_e=NeConstant, gen_time=1.394, muts_per_gen=2.5e-8)
 #add data to model
 model_inf_2histchange_temponly.set_data(sfs, length=485532)
 #set parameters to infer - contemp size, alb size, historic size (pre-alb), times of two size changes
